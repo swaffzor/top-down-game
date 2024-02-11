6d4d8f3690ff2d275fc63f178b3194f57f822516
Jeremy Swafford <jeremy@swafford.co>
2024-02-11 14:54:26 -0500

    add rotation mode

--- src/index.js
+++ src/index.js
@@ -194,17 +194,21 @@ window.addEventListener('keydown', (event) => {
         ball.direction = player.direction
         window.requestAnimationFrame(animatePowerBar)
       } else if (state.animationMode === 'powerBar') {
-        state.animationMode = 'move'
-        state.strokes++
+        state.animationMode = 'rotateBar'
         ball.velocity.x = powerBar.width / 100
         // ball.velocity.x = club.power * .52 / 100 // temporary testing
         ball.velocity.y = powerBar.height / 100
+      } else if (state.animationMode === 'rotateBar') {
+        state.animationMode = 'move'
+        state.strokes++
         window.requestAnimationFrame(animateBall)
       }
       break
     case 'Escape':
       if (state.animationMode === 'powerBar') {
         state.animationMode = 'move'
+      } else if (state.animationMode === 'rotateBar') {
+        state.animationMode = 'powerBar'
       }
       break
     case 't':
@@ -447,6 +451,9 @@ const STEP = 1
 const RUN = 2
 const OFFSET = 3
 const BAR_VELOCITY = 3
+let barDirection = 'grow'
+let barAngle = 0
+let barSpeed = 0.02
 
 const animate = () => {
   draw()
@@ -519,54 +526,102 @@ const animate = () => {
 
   document.getElementById('stat1').innerHTML = `Par ${state.par} | Strokes ${state.strokes}`
   document.getElementById('stat2').innerHTML = `ball: x: ${Math.floor(ball.position.x)}, y: ${Math.floor(ball.position.y)} z: ${Math.floor(ball.position.z)}`
-  document.getElementById('stat3').innerHTML = `shadow: x: ${Math.floor(ballShadow.position.x)}, y: ${Math.floor(ballShadow.position.y)}`
+  document.getElementById('stat3').innerHTML = `powerBar: angle: ${(barAngle / Math.PI).toFixed(2)}`
 
   window.requestAnimationFrame(animate)
 }
 
 animate()
 
-let barDirection = 'grow'
+
 
 const animatePowerBar = () => {
-  // position the power bar
-  if (player.direction === 'up') {
-    powerBar.width = 10
-    powerBar.position.x = ball.position.x - powerBar.width / 2
-    powerBar.position.y = ball.position.y - powerBar.height
-  }
-  if (player.direction === 'down') {
-    powerBar.width = 10
-    powerBar.position.x = ball.position.x - powerBar.width / 2
-    powerBar.position.y = ball.position.y - ball.height / 2
-  }
-  if (player.direction === 'left') {
-    powerBar.height = 10
-    powerBar.position.x = ball.position.x - powerBar.width
-    powerBar.position.y = ball.position.y - powerBar.height / 2
-  }
-  if (player.direction === 'right') {
-    powerBar.height = 10
-    powerBar.position.x = ball.position.x
-    powerBar.position.y = ball.position.y - powerBar.height / 2
+
+  if (state.animationMode === 'powerBar') {
+    powerBar.draw()
+    // position the power bar
+    if (player.direction === 'up') {
+      powerBar.width = 10
+      powerBar.position.x = ball.position.x - powerBar.width / 2
+      powerBar.position.y = ball.position.y - powerBar.height
+      barAngle = Math.PI
+    }
+    if (player.direction === 'down') {
+      powerBar.width = 10
+      powerBar.position.x = ball.position.x - powerBar.width / 2
+      powerBar.position.y = ball.position.y - ball.height / 2
+      barAngle = 0
+    }
+    if (player.direction === 'left') {
+      powerBar.height = 10
+      powerBar.position.x = ball.position.x - powerBar.width
+      powerBar.position.y = ball.position.y - powerBar.height / 2
+      barAngle = Math.PI
+    }
+    if (player.direction === 'right') {
+      powerBar.height = 10
+      powerBar.position.x = ball.position.x
+      powerBar.position.y = ball.position.y - powerBar.height / 2
+      barAngle = 0
+    }
+
+    const dimension = player.direction === 'up' || player.direction === 'down' ? 'height' : 'width'
+    // animate the power bar
+    if (barDirection === 'grow') {
+      if (powerBar[dimension] < club.power) powerBar[dimension] += BAR_VELOCITY
+      else barDirection = 'shrink'
+    } else if (barDirection === 'shrink') {
+      if (powerBar[dimension] > 10) powerBar[dimension] -= BAR_VELOCITY
+      else barDirection = 'grow'
+    }
+    if (powerBar.width === club.power) barDirection = 'shrink'
+    else if (powerBar.width === 0) barDirection = 'grow'
   }
 
-  powerBar.draw()
+  if (state.animationMode === 'rotateBar') {
+    console.log('rotating bar')
+    context.save(); // Save the unrotated context
+
+    if (["down", "right"].includes(player.direction)) {
+      context.translate(ball.position.x - ball.width / 4, ball.position.y + ball.height / 4); // Move the rotation point to the center of the ball
+    } else {
+      context.translate(ball.position.x + 9 / 2.1, ball.position.y - 9 / 2); // Move the rotation point to the center of the ball
+    }
+
+    // Ensure rotation lies within a given range
+    barAngle += barSpeed
+    if (player.direction === 'right' || player.direction === 'down') {
+      if (barAngle > 1 * Math.PI / 4) {
+        barSpeed = -barSpeed;
+        // file deepcode ignore DuplicateIfBody: <please specify a reason of ignoring this>
+      }
+      if (barAngle < -1 * Math.PI / 4) {
+        barSpeed = -barSpeed;
+      }
+    }
+    if (player.direction === 'left' || player.direction === 'up') {
+      if (barAngle < 3 * Math.PI / 4) {
+        barSpeed = -barSpeed;
+      }
+      if (barAngle > 5 * Math.PI / 4) {
+        barSpeed = -barSpeed;
+      }
+    }
+
+    context.rotate(barAngle); // Rotate the canvas by the desired angle
+    context.fillStyle = powerBar.fillStyle;
+    // powerBar.draw()
+    context.fillRect(0, -ball.height * 2, powerBar.width, powerBar.height);
+    context.restore(); // Restore the context back to no rotation
+  }
+
+  if (state.animationMode !== 'move') window.requestAnimationFrame(animatePowerBar)
+  else if (state.animationMode === 'move') {
+    // context.restore(); // Restore the context back to no rotation
+    // Reset current transformation matrix to the identity matrix
+    context.setTransform(1, 0, 0, 1, 0, 0);
+  }
   ball.draw()
-
-  const dimension = player.direction === 'up' || player.direction === 'down' ? 'height' : 'width'
-  // animate the power bar
-  if (barDirection === 'grow') {
-    if (powerBar[dimension] < club.power) powerBar[dimension] += BAR_VELOCITY
-    else barDirection = 'shrink'
-  } else if (barDirection === 'shrink') {
-    if (powerBar[dimension] > 10) powerBar[dimension] -= BAR_VELOCITY
-    else barDirection = 'grow'
-  }
-  if (powerBar.width === club.power) barDirection = 'shrink'
-  else if (powerBar.width === 0) barDirection = 'grow'
-
-  if (state.animationMode === 'powerBar') window.requestAnimationFrame(animatePowerBar)
 }
 
 let counter = 0
